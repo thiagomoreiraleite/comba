adf7481d46a3dbd28d1e9ffb83ad822a1f22ae34
Malin Schadel <malin.schadel@gmail.com>
2021-04-12 22:54:29 +0200

    implemented routes and controllers for gas_stations and fuel_types

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :gas_stations, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
+    resources :fuel_types, only: [:create]
+  end
 end
