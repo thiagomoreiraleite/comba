79f40523458a46214591be8ca0f22ff1c48eadae
Malin Schadel <malin.schadel@gmail.com>
2021-04-13 21:24:22 +0200

    added gas stations index view

--- app/controllers/gas_stations_controller.rb
+++ app/controllers/gas_stations_controller.rb
@@ -1,5 +1,13 @@
 class GasStationsController < ApplicationController
   def index
+    @gas_stations = GasStations.all
+
+    @markers = @gas_stations.geocoded.map do |gas_station|
+      {
+        lat: gas_station.latitude,
+        lng: gas_station.longitude
+      }
+    end
   end
 
   def show
