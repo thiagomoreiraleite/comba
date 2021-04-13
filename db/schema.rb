61cd03da7a89a84aaee276c1e59b776399a2d879
Malin Schadel <malin.schadel@gmail.com>
2021-04-13 17:38:36 +0200

    added latitude and longitude to gas_staions table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_12_203625) do
+ActiveRecord::Schema.define(version: 2021_04_13_153733) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 2021_04_12_203625) do
     t.string "brand"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "users", force: :cascade do |t|
