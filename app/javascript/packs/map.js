b2b74e2101720a4078c14e8ee20a820c3d7caa7e
Malin Schadel <malin.schadel@gmail.com>
2021-04-13 22:23:24 +0200

    added markers to map

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -9,4 +9,11 @@ if (mapElement) { // only build a map if there's a div#map to inject into
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v10'
   });
+
+  const markers = JSON.parse(mapElement.dataset.markers);
+  markers.forEach((marker) => {
+    new mapboxgl.Marker()
+      .setLngLat([ marker.lng, marker.lat ])
+      .addTo(map);
+  });
 }
