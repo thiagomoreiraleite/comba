9a19a1d7144ecae1bea8bb2a3307a4d45f630b44
Malin Schadel <malin.schadel@gmail.com>
2021-04-14 21:41:58 +0200

    implemented necessary methods in controllers

--- app/controllers/fuel_types_controller.rb
+++ app/controllers/fuel_types_controller.rb
@@ -1,4 +1,40 @@
 class FuelTypesController < ApplicationController
+  before_action :set_fuel_type, only: [:edit, :update]
+  before_action :set_gas_station_id, only: [:new, :create]
+
+  def new
+    @fuel_type = FuelType.new
+  end
+
   def create
+    @fuel_type = FuelType.new(fuel_type_params)
+
+    if @fuel_type.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @fuel_type.update(fuel_type_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def set_fuel_type
+    @fuel_type = FuelType.find(params[:id])
+  end
+
+  def set_gas_station_id
+    @gas_station = GasStation.find(params[:gas_station_id])
+  end
+
+  def fuel_type_params
+    params.require(:fuel_type).permit(:name, :price)
   end
 end
--- app/controllers/gas_stations_controller.rb
+++ app/controllers/gas_stations_controller.rb
@@ -1,8 +1,8 @@
 class GasStationsController < ApplicationController
-  skip_before_action :authenticate_user!
+  skip_before_action :authenticate_user!, only: [:index]
+  before_action :set_gas_station, only: [:edit, :update]
 
   def index
-    # @gas_stations = GasStation.where.not(latitude: nil, longitude: nil)
     @gas_stations = GasStation.all
 
     @markers = @gas_stations.geocoded.map do |gas_station|
@@ -14,21 +14,34 @@ class GasStationsController < ApplicationController
     end
   end
 
-  def show
-  end
-
   def new
+    @gas_station = GasStation.new
   end
 
   def create
+    @gas_station = GasStation.new(gas_station_params)
+    if @gas_station.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    @gas_station.update(gas_station_params)
+    redirect_to root_path
   end
 
-  def destroy
+  private
+
+  def set_gas_station
+    @gas_station = GasStation.find(params[:id])
+  end
+
+  def gas_station_params
+    params.require(:gas_station).permit(:name, :address, :brand)
   end
 end
