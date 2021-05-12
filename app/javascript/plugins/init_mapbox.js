509a2cfa2d5639c49747247ef3fbc015a2cfc56a
thiago leite <thiagomoreiraleite@gmail.com>
2021-05-12 09:09:06 -0300

    side bar buttoms

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -5,11 +5,11 @@ import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
 
-  // const fitMapToMarkers = (map, markers) => {
-  //   const bounds = new mapboxgl.LngLatBounds();
-  //   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  //   map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 1000 });
-  // };
+  const fitMapToMarkers = (map, markers) => {
+    const bounds = new mapboxgl.LngLatBounds();
+    markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+    map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 1000 });
+  };
 
   if (mapElement) {
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
@@ -45,14 +45,14 @@ const initMapbox = () => {
       new mapboxgl.Marker(el)
         .setLngLat([position.coords.longitude, position.coords.latitude])
         .addTo(map);
-      map.flyTo({
-        center: [position.coords.longitude, position.coords.latitude],
-        essential: true,
-        zoom: 12,
-        duration: 0
-      })
+      // map.flyTo({
+      //   center: [position.coords.longitude, position.coords.latitude],
+      //   essential: true,
+      //   zoom: 12,
+      //   duration: 0
+      // })
     });
-    // fitMapToMarkers(map, markers);
+    fitMapToMarkers(map, markers);
     openInfoWindow(mapMarkers);
     map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
       mapboxgl: mapboxgl }));
