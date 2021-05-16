a60619aad45fb0c41e24f7e139bb4f4f6460c711
thiago leite <thiagomoreiraleite@gmail.com>
2021-05-16 12:34:37 -0300

    include user loc back

--- app/controllers/gas_stations_controller.rb
+++ app/controllers/gas_stations_controller.rb
@@ -9,7 +9,9 @@ class GasStationsController < ApplicationController
     if client_ip == "::1"
       client_ip = "Fortaleza, Ceara"
     end
-    @user_loc = Geocoder.search(client_ip).first.coordinates
+    lat = Geocoder.search(client_ip).first.latitude
+    lon = Geocoder.search(client_ip).first.longitude
+    @user_loc = [lat, lon]
     @gas_stations = GasStation.where.not(latitude: nil, longitude: nil)
     @gasolina_stations = @gas_stations.includes(:fuel_types).where(fuel_types: { name: "Gasolina" }).or(@gas_stations.includes(:fuel_types).where(fuel_types: { name: "Gasolina Aditivada" })).order("fuel_types.price ASC")
     @diesel_stations = @gas_stations.includes(:fuel_types).where(fuel_types: { name: "Diesel" }).or(@gas_stations.includes(:fuel_types).where(fuel_types: { name: "Diesel S10" })).order("fuel_types.price ASC")
