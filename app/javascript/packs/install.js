0fb5177f47f07cbf670bf58382adff796ab0e67b
thiago leite <thiagomoreiraleite@gmail.com>
2021-05-24 09:47:39 -0300

    further set ups pwa

--- app/javascript/packs/install.js
+++ app/javascript/packs/install.js
@@ -3,34 +3,60 @@ const install = () =>{
     const addBtn = document.querySelector('#install');
     addBtn.style.display = 'none';
 
+    // window.addEventListener('beforeinstallprompt', (e) => {
+    //     // Prevent the mini-infobar from appearing on mobile
+    //     e.preventDefault();
+    //     // Stash the event so it can be triggered later.
+    //     deferredPrompt = e;
+    //     // Update UI notify the user they can install the PWA
+    //     // showInstallPromotion();
+    //     // Optionally, send analytics event that PWA install promo was shown.
+    //     addBtn.style.display = 'block';
+    //     console.log(`'beforeinstallprompt' event was fired.`);
+        
+    //     addBtn.addEventListener('click', async () => {
+    //         // Hide the app provided install promotion
+    //         // hideInstallPromotion();
+    //         // Show the install prompt
+    //         deferredPrompt.prompt();
+    //         // Wait for the user to respond to the prompt
+    //         const { outcome } = await deferredPrompt.userChoice;
+    //         // Optionally, send analytics event with outcome of user choice
+    //         console.log(`User response to the install prompt: ${outcome}`);
+    //         // We've used the prompt, and can't use it again, throw it away
+    //         deferredPrompt = null;
+    //     });
+    // });
+
+
     window.addEventListener('beforeinstallprompt', (e) => {
-        // Prevent the mini-infobar from appearing on mobile
+        // Prevent Chrome 67 and earlier from automatically showing the prompt
         e.preventDefault();
         // Stash the event so it can be triggered later.
         deferredPrompt = e;
-        // Update UI notify the user they can install the PWA
-        showInstallPromotion();
-        // Optionally, send analytics event that PWA install promo was shown.
+        // Update UI to notify the user they can add to home screen
         addBtn.style.display = 'block';
         console.log(`'beforeinstallprompt' event was fired.`);
-        
-        buttonInstall.addEventListener('click', async () => {
-            // Hide the app provided install promotion
-            hideInstallPromotion();
-            // Show the install prompt
+      
+        addBtn.addEventListener('click', () => {
+            // hide our user interface that shows our A2HS button
+            addBtn.style.display = 'none';
+            // Show the prompt
             deferredPrompt.prompt();
             // Wait for the user to respond to the prompt
-            const { outcome } = await deferredPrompt.userChoice;
-            // Optionally, send analytics event with outcome of user choice
-            console.log(`User response to the install prompt: ${outcome}`);
-            // We've used the prompt, and can't use it again, throw it away
+            deferredPrompt.userChoice.then((choiceResult) => {
+            if (choiceResult.outcome === 'accepted') {
+                console.log('User accepted the A2HS prompt');
+            } else {
+                console.log('User dismissed the A2HS prompt');
+            }
             deferredPrompt = null;
+            });
         });
     });
-
     window.addEventListener('appinstalled', () => {
         // Hide the app-provided install promotion
-        hideInstallPromotion();
+        // hideInstallPromotion();
         // Clear the deferredPrompt so it can be garbage collected
         deferredPrompt = null;
         // Optionally, send analytics event to indicate successful install
