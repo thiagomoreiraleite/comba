72071e138f2d482e2160cea8e0ba7276005e87d3
thiago leite <thiagomoreiraleite@gmail.com>
2021-05-25 10:17:16 -0300

    button install

--- app/javascript/packs/install.js
+++ app/javascript/packs/install.js
@@ -3,32 +3,6 @@ const install = () =>{
     const addBtn = document.querySelector('#install');
     addBtn.style.display = 'none';
 
-    // window.addEventListener('beforeinstallprompt', (e) => {
-    //     // Prevent the mini-infobar from appearing on mobile
-    //     e.preventDefault();
-    //     // Stash the event so it can be triggered later.
-    //     deferredPrompt = e;
-    //     // Update UI notify the user they can install the PWA
-    //     // showInstallPromotion();
-    //     // Optionally, send analytics event that PWA install promo was shown.
-    //     addBtn.style.display = 'block';
-    //     console.log(`'beforeinstallprompt' event was fired.`);
-        
-    //     addBtn.addEventListener('click', async () => {
-    //         // Hide the app provided install promotion
-    //         // hideInstallPromotion();
-    //         // Show the install prompt
-    //         deferredPrompt.prompt();
-    //         // Wait for the user to respond to the prompt
-    //         const { outcome } = await deferredPrompt.userChoice;
-    //         // Optionally, send analytics event with outcome of user choice
-    //         console.log(`User response to the install prompt: ${outcome}`);
-    //         // We've used the prompt, and can't use it again, throw it away
-    //         deferredPrompt = null;
-    //     });
-    // });
-
-
     window.addEventListener('beforeinstallprompt', (e) => {
         // Prevent Chrome 67 and earlier from automatically showing the prompt
         e.preventDefault();
@@ -53,14 +27,6 @@ const install = () =>{
             deferredPrompt = null;
             });
         });
-    });
-    window.addEventListener('appinstalled', () => {
-        // Hide the app-provided install promotion
-        // hideInstallPromotion();
-        // Clear the deferredPrompt so it can be garbage collected
-        deferredPrompt = null;
-        // Optionally, send analytics event to indicate successful install
-        console.log('PWA was installed');
     });
 }
 export { install };
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -16,6 +16,14 @@
 const { resolve } = require('path');
 const { config, environment, Environment } = require('@rails/webpacker');
 environment.loaders.delete('nodeModules');
+const webpack = require('webpack');
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: ['popper.js', 'default']
+  })
+);
 const WebpackerPwa = require('webpacker-pwa');
 new WebpackerPwa(config, environment);
 module.exports = environment;
